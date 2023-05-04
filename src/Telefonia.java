e2f02887162177ef35fc4395f31dd62bacf9ee9a
Thiagod3 <thiago.vilela3@outlook.com>
2023-05-04 17:19:37 -0300

    metodos_update

--- src/Telefonia.java
+++ src/Telefonia.java
@@ -87,27 +87,186 @@ public class Telefonia {
 		}
 	
 	public void ListarAssinantes() {
-		
+		System.out.println("Assinantes de Pré-Pago:");
+		for (int i = 0; i < numPrePagos; i++) {
+			PrePago prePago = prePagos[i];
+			System.out.println(prePago.toString());
+		}
+
+		System.out.println("Assinantes de Pós-Pago:");
+		for (int i = 0; i < numPosPagos; i++) {
+			PosPago posPago = posPagos[i];
+			System.out.println(posPago.toString());
+		}
 	}
 	
 	public void FazerChamada() {
-		
+		Scanner scanner = new Scanner(System.in);
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		GregorianCalendar calendar = new GregorianCalendar();
+		boolean foiNull = false;
+		do{
+			int tipo;
+				do{
+					System.out.println("+---------------------------+");
+					System.out.println("|                           |");
+					System.out.println("|     Selecione o tipo      |");
+					System.out.println("|       de Assinatura       |");
+					System.out.println("|                           |");
+					System.out.println("|   1. Pré-pago             |");
+					System.out.println("|   2. Pós-pago             |");
+					System.out.println("|                           |");
+					System.out.println("+---------------------------+");
+					tipo = scanner.nextInt();
+				} while(tipo !=1 && tipo !=2 );
+
+			System.out.println("Informe o CPF do assinante:");
+			long cpf = scanner.nextLong();
+			scanner.nextLine();
+
+			if(tipo == 1){
+				PrePago prePago = LocalizarPrePago(cpf);
+
+				if(prePago != null){
+					System.out.println("\nInforme a data da chamada (dd/MM/yyyy):");
+					String dataStr = scanner.nextLine();
+					
+					Date data = null;
+					boolean foi = false;
+
+					while(!foi){
+						try {
+							data = dateFormat.parse(dataStr);
+							foi = true;
+						} catch (ParseException e) {
+							System.out.println("\nA data não foi informada no formato correto");
+							System.out.println("Por favor, informe a data da chamada no formato dd/MM/yyyy:");
+							dataStr = scanner.nextLine();
+						}
+					}
+
+					calendar.setTime(data);
+
+					System.out.println("\nInforme a duração da chamada no seguinte formato:");
+					int duracao = scanner.nextInt();
+					scanner.nextLine();
+
+					prePago.FazerChamada(calendar, duracao);
+				}else{
+					System.out.println("O CPF não consta no registro de assinantes Pré-pago.");
+					foiNull = true;
+				}
+			}else{			
+				PosPago posPago = LocalizarPosPago(cpf);
+
+				if(posPago != null){
+					System.out.println("\nInforme a data da chamada (dd/MM/yyyy):");
+					String dataStr = scanner.nextLine();
+					
+					Date data = null;
+					boolean foi = false;
+
+					while(!foi){
+						try {
+							data = dateFormat.parse(dataStr);
+							foi = true;
+						} catch (ParseException e) {
+							System.out.println("\nA data não foi informada no formato correto");
+							System.out.println("Por favor, informe a data da chamada no formato dd/MM/yyyy:");
+							dataStr = scanner.nextLine();
+						}
+					}
+
+					calendar.setTime(data);
+
+					System.out.println("\nInforme a duração em minutos da chamada:");
+					int duracao = scanner.nextInt();
+					scanner.nextLine();
+
+					posPago.FazerChamada(calendar, duracao);
+				}else{
+					System.out.println("O CPF não consta no registro de assinantes Pós-pago.");
+					foiNull = true;
+				}
+			}	
+		}while(foiNull == true);
+	
 	}
 	
 	public void FazerRecarga() {
+		Scanner scanner = new Scanner(System.in);
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		GregorianCalendar calendar = new GregorianCalendar();
 		
+		boolean foiNull = false;
+
+		do{
+			System.out.println("Informe o CPF do assinante:");
+			long cpf = scanner.nextLong();
+
+				PrePago prePago = LocalizarPrePago(cpf);
+
+				if(prePago != null){
+					System.out.println("\nInforme a data da recarga (dd/MM/yyyy):");
+					String dataStr = scanner.nextLine();
+					
+					Date data = null;
+					boolean foi = false;
+
+					while(!foi){
+						try {
+							data = dateFormat.parse(dataStr);
+							foi = true;
+						} catch (ParseException e) {
+							System.out.println("\nA data não foi informada no formato correto");
+							System.out.println("Por favor, informe a data da recarga no formato dd/MM/yyyy:");
+							dataStr = scanner.nextLine();
+						}
+					}
+
+					calendar.setTime(data);
+
+					System.out.println("\nInforme o valor da recarga");
+					float valor = scanner.nextFloat();
+					scanner.nextLine();
+
+					prePago.Recarregar(calendar, valor);
+				}else{
+					System.out.println("O CPF não consta no registro de assinantes Pré-pago.");
+					foiNull = true;
+				}
+			
+		}while(foiNull == true);
 	}
 	
+	public void ImprimirFatura() {
+		
+	}
+
 	private PrePago LocalizarPrePago(long cpf) {
+
+		for (int i = 0; i < numPrePagos; i++){
+			PrePago prePago = prePagos[i];
+
+			if(cpf == prePago.getCpf()){
+				return prePago;
+			}
+		}
+
 		return null;
 	}
 	
 	private PosPago LocalizarPosPago(long cpf) {
+
+		for (int i = 0; i < numPrePagos; i++){
+			PosPago posPago = posPagos[i];
+
+			if(cpf == posPago.getCpf()){
+				return posPago;
+			}
+		}
+
 		return null;
-		
-	}
-	
-	public void ImprimirFatura() {
 		
 	}
 
